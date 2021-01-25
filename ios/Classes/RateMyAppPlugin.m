449714f496e3a627cb3a49537912499d2f86494f
Jang-Ho Hwang <rath@xrath.com>
2021-01-26 02:43:00 +0900

    Port from Swift to Objective-C

--- ios/Classes/RateMyAppPlugin.m
+++ ios/Classes/RateMyAppPlugin.m
@@ -1,15 +1,36 @@
 #import "RateMyAppPlugin.h"
-#if __has_include(<rate_my_app/rate_my_app-Swift.h>)
-#import <rate_my_app/rate_my_app-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "rate_my_app-Swift.h"
-#endif
+#import <StoreKit/StoreKit.h>
 
 @implementation RateMyAppPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftRateMyAppPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"rate_my_app"
+            binaryMessenger:[registrar messenger]];
+  RateMyAppPlugin * instance = [[RateMyAppPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  NSString *m = call.method;
+  if ([m isEqualToString:@"launchNativeReviewDialog"]) {
+    [SKStoreReviewController requestReview];
+    result([NSNumber numberWithBool:YES]);
+  } else if ([m isEqualToString:@"isNativeDialogSupported"]) {
+    result([NSNumber numberWithBool:YES]);
+  } else if ([m isEqualToString:@"launchStore"]) {
+    NSString *appId = call.arguments[@"appId"];
+    if (appId == nil || appId.length == 0) {
+      result(@2);
+      return;
+    }
+    NSString *link = [NSString stringWithFormat:@"itms-apps://itunes.apple.com/app/id/%@?action=write-review", appId];
+    NSURL *url = [[NSURL alloc] initWithString:link];
+    if ([UIApplication.sharedApplication canOpenURL:url]) {
+      [UIApplication.sharedApplication openURL:url options:@{} completionHandler:nil];
+      result(@1);
+    } else {
+      result(@2);
+    }
+  }
 }
 @end
--- ios/rate_my_app.podspec
+++ ios/rate_my_app.podspec
@@ -4,20 +4,19 @@
 #
 Pod::Spec.new do |s|
   s.name             = 'rate_my_app'
-  s.version          = '0.7.1'
+  s.version          = '0.7.2'
   s.summary          = 'Allows to kindly ask users to rate your app if custom conditions are met (eg. install time, number of launches, etc...).'
   s.description      = <<-DESC
 Allows to kindly ask users to rate your app if custom conditions are met (eg. install time, number of launches, etc...).
                        DESC
-  s.homepage         = 'https://github.com/Skyost/RateMyApp'
+  s.homepage         = 'https://github.com/rath/RateMyApp'
   s.license          = { :file => '../LICENSE' }
   s.author           = { 'Skyost' => 'me@skyost.eu' }
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
-  s.platform = :ios, '8.0'
+  s.platform = :ios, '11.0'
 
   # Flutter.framework does not contain a i386 slice. Only x86_64 simulators are supported.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'VALID_ARCHS[sdk=iphonesimulator*]' => 'x86_64' }
-  s.swift_version = '5.0'
 end
